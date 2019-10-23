9f38ace5cb0a932238ee572179c2a9295200c1a3
Chelladurai <acdurai04@gmail.com>
2019-10-23 16:23:05 -0500

    Update ProgressAllTestsSuite.java

--- src/test/java/com/cd/ProgressAllTestsSuite.java
+++ src/test/java/com/cd/ProgressAllTestsSuite.java
@@ -1,10 +1,12 @@
 package com.cd;
 
 import org.jenkinsci.testinprogress.runner.ProgressSuite;
+
+import org.jenkinsci.testinprogress.runner.ProgressBatchSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(ProgressSuite.class)
+@RunWith(ProgressBatchSuite.class)
 @SuiteClasses({ AppTest.class})
 public class ProgressAllTestsSuite {
 
